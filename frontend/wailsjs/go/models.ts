8c32a21fdb8fab57057fa2eb43c6c41ea49795b9
jokyu yukihito <ok220020@oks.iput.ac.jp>
2026-08-09 15:07:27 +0900

    🔧 実験ブリーフメッセージ送信のWailsフロントエンドバインディングを生成 #5

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -267,6 +267,50 @@ export namespace wails {
 		}
 	}
 	
+	export class SendExperimentBriefMessageData {
+	    operationId: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new SendExperimentBriefMessageData(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.operationId = source["operationId"];
+	    }
+	}
+	export class SendExperimentBriefMessageResponse {
+	    data?: SendExperimentBriefMessageData;
+	    error?: ErrorResponse;
+	
+	    static createFrom(source: any = {}) {
+	        return new SendExperimentBriefMessageResponse(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.data = this.convertValues(source["data"], SendExperimentBriefMessageData);
+	        this.error = this.convertValues(source["error"], ErrorResponse);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
 	export class StartExperimentBriefingData {
 	    briefingSessionId: string;
 	    operationId: string;
